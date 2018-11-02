db0e0c2171590ec5b2917c24f98d01d0ef9175aa
ognjen <ognjen@ballab.com>
2018-11-02 10:36:10 +0100

    Initial commit

---
+++ src/main/java/com/championsleague/championsleague/ChampionsLeagueApplication.java
@@ -0,0 +1,12 @@
+package com.championsleague.championsleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionsLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampionsLeagueApplication.class, args);
+	}
+}
